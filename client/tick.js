8fd306b7aba96582bbecf75df4f14df37134fa79
Mick Muzac <mmuzac23@gmail.com>
2013-07-26 09:32:35 -0400

    Initial commit

---
+++ client/tick.js
@@ -0,0 +1,34 @@
+var $tickjs = new function(){
+
+	var self = this;
+	var master = false;
+	var ticks = 0;
+	var hours = 0;
+	
+	//Number of milliseconds per tick
+	self.tickInterval = 10; 
+
+	self.sync = function(){
+
+
+
+	};
+
+	self.data = {};
+	
+	self.start = function(server){
+	
+		
+	};
+	
+	self.stop = function(){
+	
+	
+	};
+
+
+};
+
+if(!tick){
+	var tick = $tickjs;
+}
